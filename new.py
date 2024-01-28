e03b49e884f032af91c0ad5c4307ccc0ce39598b
GuptaJi000 <155901036+GuptaJi000@users.noreply.github.com>
2024-01-28 10:57:10 +0530

    Update new.py
    Remove comment or extra 2 lines

--- new.py
+++ new.py
@@ -1,27 +1,24 @@
 num1 = int(input("Enter your first number: "))
 operator = (input("Enter your operator: "))
 num2 = int(input("Enter your second number: "))
-# num1 = int(num1)
-# num2 = int(num2)
 
-
-if operator == "+": # Here will perform task with two operands with one operator.
+if operator == "+": 
     print(num1 + num2)
     
-elif operator == "-":# Here will perform task with two operands with one operator.
+elif operator == "-":
     print(num1 - num2)
    
-elif operator == "/":# Here will perform task with two operands with one operator.
+elif operator == "/":
     print(num1 / num2)
     
 
-elif operator == "*":# Here will perform task with two operands with one operator.
+elif operator == "*":
     print(num1 * num2)
     
-elif operator == "%":# Here will perform task with two operands with one operator.
+elif operator == "%":
     print(num1 % num2)
 
-elif operator == "^":# Here will perform task with two operands with one operator.
+elif operator == "^":
     print(num1 ** num2)
 
 else:
